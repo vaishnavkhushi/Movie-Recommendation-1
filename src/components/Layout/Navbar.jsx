725b0be2de13b8cbbb1aee396848e66ab71d7029
khushi <khushi@khushis-MacBook-Pro-3.local>
2026-04-02 10:48:15 +0530

    Rename header to net-flix and set Trending Now section background to bg-bgpalette

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           <div className="flex items-center">
             <Link to="/" className="flex items-center space-x-2">
               <span className="text-xl font-bold text-white">
-                Movie Explorer
+                net-flix
               </span>
             </Link>
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -92,7 +92,7 @@ const Home = () => {
       )}
 
       {/* Popular Movies Section */}
-      <section className="py-16 px-4 sm:px-6 lg:px-8">
+      <section className="py-16 px-4 sm:px-6 lg:px-8 bg-bgpalette">
         <div className="max-w-7xl mx-auto">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
